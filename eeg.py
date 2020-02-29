39dee39fef1d9ca088a76ce6ffea17955bd488c5
Zernach <ryan@zernach.com>
2020-02-28 21:43:49 -0500

    march 1st updates

--- eeg.py
+++ eeg.py
@@ -2,7 +2,8 @@ from flask import Flask, json
 from flask_cors import CORS
 import pandas as pd
 from sklearn.externals import joblib
-from sklearn.neighbors import NearestNeighbors
+from sklearn import pipeline
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
 
@@ -12,20 +13,56 @@ def make_app():
     @app.route('/id/<int:post_id>')
 
     def home(post_id)
-
         ms = post_id
-        txt = connect_eeg_to_txt(ms)
+        # Lines 18-53 copied from https://brainflow.readthedocs.io/en/stable/Examples.html
+        parser = argparse.ArgumentParser ()
+        # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+        parser.add_argument ('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
+        parser.add_argument ('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
+        parser.add_argument ('--ip-address', type = str, help  = 'ip address', required = False, default = '')
+        parser.add_argument ('--serial-port', type = str, help  = 'serial port', required = False, default = '')
+        parser.add_argument ('--mac-address', type = str, help  = 'mac address', required = False, default = '')
+        parser.add_argument ('--other-info', type = str, help  = 'other info', required = False, default = '')
+        parser.add_argument ('--streamer-params', type = str, help  = 'other info', required = False, default = '')
+        parser.add_argument ('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
+        parser.add_argument ('--log', action = 'store_true')
+        args = parser.parse_args ()
+
+        params = BrainFlowInputParams ()
+        params.ip_port = args.ip_port
+        params.serial_port = args.serial_port
+        params.mac_address = args.mac_address
+        params.other_info = args.other_info
+        params.ip_address = args.ip_address
+        params.ip_protocol = args.ip_protocol
+
+        if (args.log):
+            BoardShim.enable_dev_board_logger ()
+        else:
+            BoardShim.disable_board_logger ()
+
+        board = BoardShim (args.board_id, params)
+        board.prepare_session ()
+
+        # board.start_stream () # use this for default options
+        board.start_stream (ms, args.streamer_params)
+        time.sleep (10)
+        # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+        data = board.get_board_data () # get all data and remove it from internal buffer
+        board.stop_stream ()
+        board.release_session ()
 
         column_names = ['index', 'channel1','channel2', 'channel3', 'channel4', 'accel1', 'accel2', 'accel3', 'timestamp', 'aux']
         dropped_row_indices = [0, 1, 2, 3, 4, 5]
 
-        df = pd.read_csv(txt, sep=',', header=None, names=column_names)
+        df = pd.read_csv(data, sep=',', header=None, names=column_names)
         df = df.drop(dropped_row_indices, axis=0).reset_index()
         df = df.drop(['level_0', 'index'], axis=1)
 
         # Is my destination address formatted correctky?
         model = joblib.load('flask_test/rfc.joblib')
-        
+
+        #commands = model.predict_proba(df)
         commands = model.predict(df)
         commands_df = pd.DataFrame({'index': commands.index, 'predictions':commands})
         commands_df['predictions'] = commands_df['predictions'].astype('int64')
