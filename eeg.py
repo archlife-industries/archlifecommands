8ce28eb2b942d2e883532f69b8b9c5567a8aa39b
Zernach <ryan@zernach.com>
2020-02-28 21:46:22 -0500

    added import statements

--- eeg.py
+++ eeg.py
@@ -5,6 +5,12 @@ from sklearn.externals import joblib
 from sklearn import pipeline
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import argparse
+import time
+import numpy as np
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 
 def make_app():
@@ -17,18 +23,18 @@ def make_app():
         # Lines 18-53 copied from https://brainflow.readthedocs.io/en/stable/Examples.html
         parser = argparse.ArgumentParser ()
         # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-        parser.add_argument ('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
-        parser.add_argument ('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
-        parser.add_argument ('--ip-address', type = str, help  = 'ip address', required = False, default = '')
-        parser.add_argument ('--serial-port', type = str, help  = 'serial port', required = False, default = '')
-        parser.add_argument ('--mac-address', type = str, help  = 'mac address', required = False, default = '')
-        parser.add_argument ('--other-info', type = str, help  = 'other info', required = False, default = '')
-        parser.add_argument ('--streamer-params', type = str, help  = 'other info', required = False, default = '')
-        parser.add_argument ('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
-        parser.add_argument ('--log', action = 'store_true')
-        args = parser.parse_args ()
+        parser.add_argument('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
+        parser.add_argument('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
+        parser.add_argument('--ip-address', type = str, help  = 'ip address', required = False, default = '')
+        parser.add_argument('--serial-port', type = str, help  = 'serial port', required = False, default = '')
+        parser.add_argument('--mac-address', type = str, help  = 'mac address', required = False, default = '')
+        parser.add_argument('--other-info', type = str, help  = 'other info', required = False, default = '')
+        parser.add_argument('--streamer-params', type = str, help  = 'other info', required = False, default = '')
+        parser.add_argument('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
+        parser.add_argument('--log', action = 'store_true')
+        args = parser.parse_args()
 
-        params = BrainFlowInputParams ()
+        params = BrainFlowInputParams()
         params.ip_port = args.ip_port
         params.serial_port = args.serial_port
         params.mac_address = args.mac_address
@@ -37,20 +43,20 @@ def make_app():
         params.ip_protocol = args.ip_protocol
 
         if (args.log):
-            BoardShim.enable_dev_board_logger ()
+            BoardShim.enable_dev_board_logger()
         else:
-            BoardShim.disable_board_logger ()
+            BoardShim.disable_board_logger()
 
-        board = BoardShim (args.board_id, params)
-        board.prepare_session ()
+        board = BoardShim(args.board_id, params)
+        board.prepare_session()
 
         # board.start_stream () # use this for default options
         board.start_stream (ms, args.streamer_params)
-        time.sleep (10)
+        time.sleep(10)
         # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-        data = board.get_board_data () # get all data and remove it from internal buffer
-        board.stop_stream ()
-        board.release_session ()
+        data = board.get_board_data() # get all data and remove it from internal buffer
+        board.stop_stream()
+        board.release_session()
 
         column_names = ['index', 'channel1','channel2', 'channel3', 'channel4', 'accel1', 'accel2', 'accel3', 'timestamp', 'aux']
         dropped_row_indices = [0, 1, 2, 3, 4, 5]
