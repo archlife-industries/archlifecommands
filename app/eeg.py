f3916fa678204db7d35c17fa415b9bd3db21d1b9
Your Name <you@example.com>
2020-04-06 07:57:16 -0500

    updated Archlife API

--- app/eeg.py
+++ app/eeg.py
@@ -9,7 +9,6 @@ import numpy as np
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
-#import os
 
 
 def make_app():
@@ -24,54 +23,36 @@ def make_app():
         # Lines 27-65 copied from https://brainflow.readthedocs.io/en/stable/Examples.html
         # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
-        parser.add_argument('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
-        parser.add_argument('--ip-address', type = str, help  = 'ip address', required = False, default = '')
-        parser.add_argument('--serial-port', type = str, help  = 'serial port', required = False, default = '')
-        parser.add_argument('--mac-address', type = str, help  = 'mac address', required = False, default = '')
-        parser.add_argument('--other-info', type = str, help  = 'other info', required = False, default = '')
-        parser.add_argument('--streamer-params', type = str, help  = 'other info', required = False, default = '')
-        parser.add_argument('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
-        parser.add_argument('--log', action = 'store_true')
-        args = parser.parse_args()
-
         params = BrainFlowInputParams()
-        params.ip_port = args.ip_port
-        params.serial_port = args.serial_port
-        params.mac_address = args.mac_address
-        params.other_info = args.other_info
-        params.ip_address = args.ip_address
-        params.ip_protocol = args.ip_protocol
-
-        if (args.log):
-            BoardShim.enable_dev_board_logger()
-        else:
-            BoardShim.disable_board_logger()
-
-        board = BoardShim(args.board_id, params)
+        params.board_id = 1
+        params.serial_port = "COM4"
+        params.mac_address = "DC:3D:46:E5:10:99"
+        
+        board = BoardShim(1, params)
         board.prepare_session()
 
-        board.start_stream () # use this for default options
-        board.start_stream (ms, args.streamer_params)
-        time.sleep(3000/1000)
-        data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-        data = board.get_board_data() # get all data and remove it from internal buffer
+        board.start_stream() # use this for default options
+        data = board.get_current_board_data(256) # get latest 256 packages or less, doesnt remove them from internal buffer
         board.stop_stream()
         board.release_session()
 
+        # return 'a string'
 
         # CONNECT THE DATA TO THE MODEL FOR PREDICTIONS
 
         # Test Sample Data
-        #data = 'https://archlife.org/wp-content/uploads/2020/03/OpenBCI-RAW-right0.txt'
+        # data = 'https://archlife.org/wp-content/uploads/2020/03/OpenBCI-RAW-right0.txt'
 
-        column_names = ['index', 'channel1','channel2', 'channel3', 'channel4', 'accel1', 'accel2', 'accel3', 'timestamp', 'aux']
-        dropped_row_indices = [0, 1, 2, 3, 4, 5]
+        # column_names = ['index', 'channel1','channel2', 'channel3', 'channel4', 'accel1', 'accel2', 'accel3', 'timestamp', 'aux']
+        # dropped_row_indices = [0, 1, 2, 3, 4, 5]
 
-        df = pd.read_csv(data, sep=',', header=None, names=column_names)
-        df = df.drop(dropped_row_indices, axis=0).reset_index()
-        df = df.drop(['level_0', 'index', 'timestamp'], axis=1)
+        # df = pd.DataFrame(data=data, columns=column_names)
+
+        # df = df.drop(dropped_row_indices, axis=0).reset_index()
+        # df = df.drop(['level_0', 'index', 'timestamp'], axis=1)
+
+        df = pd.DataFrame({'channel1': data[0], 'channel2': data[1], 'channel3': data[2], 'channel4': data[3], 'accel1': data[4], 'accel2': data[5], 'accel3': data[6], 'aux': data[8]})
+        
         df = df.dropna(axis=0)
 
         model = joblib.load('flask_test/rfc.joblib')
@@ -89,17 +70,3 @@ def make_app():
         return str(command_pred)
 
     return app
-
-        #return "server response 200"
-
-    #@app.route('/command/')
-    #def predict():
-        #ms = post_id
-
-
-
-
-# r = requests.get(url = URL, params = PARAMS)
-
-#export FLASK_APP=app:make_app
-#flask run
